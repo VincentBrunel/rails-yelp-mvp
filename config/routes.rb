c804f170fb02b938b71ca54c5cd6954c08216cca
vincent brunel <vincent.brunel20@gmail.com>
2022-08-18 10:39:21 +0200

    oui

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,21 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
+  get 'restaurants', to: 'restaurants#index'
+  post 'restaurants', to: 'restaurants#create'
+  get 'restaurants/new', to: 'restaurants#new'
+  get 'restaurants/:id', to: 'restaurants#show'
+  get '/restaurants/:restaurant_id/reviews/new', to: 'reviews#new'
+  post 'restaurants/:restaurant_id/reviews', to: 'reviews#create'
+  resources :restaurants do
+    collection do
+      get :review
+    end
+  end 
+  resources :restaurants do
+    member do
+     get 'new'
+    end
+  end
+
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+boccaccio = Restaurant.create([{name: "Boccaccio", address: "2 rue jacoulet", phone_number: "0258369874", category: "italian"}])
+chez_justine = Restaurant.create([{name: "Chez Justine", address: "15 rue jacoulet", phone_number: "0244449874", category: "french"}])
+sushi = Restaurant.create([{name: "Sushi", address: "22 rue jacoulet", phone_number: "0789879874", category: "japanese"}])
+shangai = Restaurant.create([{name: "Shangaï", address: "5 blv pépé", phone_number: "0154698788", category: "chinese"}])
+fritas = Restaurant.create([{name: "fritas", address: "pas loin", phone_number: "0154789666", category: "belgian"}])
